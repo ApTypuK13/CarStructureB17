82e3aa5763116023921bf3f770c746c774815a73
Artur <arthur131092@gmail.com>
2023-11-04 12:05:03 -0400

    i have added all the features required for the car

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,10 +1,17 @@
 public class main {
     public static void main(String[] args) {
 
-        CarFeatures audi=new CarFeatures();
-        audi.color="black";
-        audi.Hp="5000";
-        audi.Doors="2";
-        audi.Model="A90";
+        CarFeatures audi = new CarFeatures();
+        audi.color = "black";
+        audi.Hp = "5000";
+        audi.Doors = "2";
+        audi.Model = "A90";
+
+        CarFeature1 tire = new CarFeature1();
+        tire.tyre = "4";
+        tire.tyrecolor = "blue";
+        tire.tyreModel="Yokohama";
+        tire.tyreWidth="18";
+
     }
 }
